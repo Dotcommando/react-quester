bdfa4cac4d66b4c0e5e6dbb6a863c0b14fcd0db8
Mikhail Filchushkin <mikhail.filchushkin@eventplatform.ru>
2018-04-30 20:15:56 +0300

    Init API for Node.js + MongoDB.

---
+++ api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/users', function(req, res) {
+    res.send({type: 'GET'});
+});
+
+router.post('/users', function(req, res) {
+    res.send({type: 'POST'});
+});
+
+router.put('/users/:id', function(req, res) {
+    res.send({type: 'PUT'});
+});
+
+router.delete('/users/:id', function(req, res) {
+    res.send({type: 'DELETE'});
+});
+
+module.exports = router;
