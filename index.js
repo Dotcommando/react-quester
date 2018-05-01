9ba638ff2fe0eb6b9f84ad2bbcb10ad99b0904e1
Mikhail Filchushkin <mikhail.filchushkin@eventplatform.ru>
2018-05-01 13:39:46 +0300

    Error handling completed.

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.use(bodyParser.json());
 
 app.use('/api', require("./routes/api"));
 
+app.use(function(err, req, res, next){
+    console.log(err);
+    res.status(422).send({error: err.message});
+});
+
 app.listen(process.env.port || 4000, function(){
     console.log('Now listening for requests...');
 });
--- routes/api.js
+++ routes/api.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.get('/users', function(req, res) {
+router.get('/users', function(req, res, next) {
     res.send({type: 'GET'});
 });
 
-router.post('/users', function(req, res) {
+router.post('/users', function(req, res, next) {
     // var user = new User(req.body);
     // user.save();
     User.create(req.body).then(function(user){
         res.send(user);
-    });
+    }).catch(next);
 });
 
-router.put('/users/:id', function(req, res) {
+router.put('/users/:id', function(req, res, next) {
     res.send({type: 'PUT'});
 });
 
-router.delete('/users/:id', function(req, res) {
+router.delete('/users/:id', function(req, res, next) {
     res.send({type: 'DELETE'});
 });
 
