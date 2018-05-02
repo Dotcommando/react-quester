749ee872799b41291f25483377bd2c9139456958
Mikhail Filchushkin <mikhail.filchushkin@eventplatform.ru>
2018-05-02 11:27:24 +0300

    Gets longitude and latitude from params.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 mongoose.connect('mongodb://localhost/users');
 mongoose.Promise = global.Promise;
 
+app.use(express.static('public'));
+
 app.use(bodyParser.json());
 
 app.use('/api', require("./routes/api"));
--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,21 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.get('/users', function(req, res, next) {
-    res.send({type: 'GET'});
+    //res.send({type: 'GET'});
+    User.aggregate().near({
+        near: [parseFloat(req.query.lng), parseFloat(req.query.lat)],
+        maxDistance: 0.9, // radians
+        spherical: true,
+        distanceField: "dist.calculated"
+    })﻿.then(function(users){
+        res.send(users);
+    });
 });
+
+
+// router.get('/users', function(req, res, next) {
+//     res.send({type: 'GET'});
+// });
 
 router.post('/users', function(req, res, next) {
     // var user = new User(req.body);
